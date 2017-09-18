0278ac3c08356a4409e673383a2c20f6ed0dacf8
notifiqueifes <32045808+notifiqueifes@users.noreply.github.com>
2017-09-17 22:36:11 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Notifique</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" href="C:\Users\PC\Documents\João Antônio\2017\Português\Site\Imagens\favicon.png" type="image/x-png">
+	<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+<style>
+
+	@font-face {
+     font-family: square;
+     src: url('https://joaoantoniosc.github.io/fontes/Square.ttf');
+}
+/*****************NAV*******************/
+	.navembaixo{
+		position: fixed!important;
+		bottom: 0px; 
+		background-color: #F2F2F2 !important;
+		color: #848484 !important;
+		border-top: solid rgba(255, 255, 255, 0.3);
+		text-align: center;
+		width: 100%;
+		font-size: 24px;
+	}
+	.naveg {
+		background-color: #F2F2F2 !important;
+		color: #848484 !important;
+		text-align: center;
+		font-size: 24px;
+		letter-spacing: 7vw;
+	}
+	.naveg:hover {
+		color: #1B4411;	
+		text-shadow: 0 0 2px #1B4411;	
+	}
+/**************HEAD******************/
+	.logo{
+		width: 185px;
+		height: 46px;
+	}
+
+	.spacer{
+		padding: 1%;
+	}
+/********************BLOCO************/
+		.block {
+    	opacity: 1;
+  		width: 100%;
+  		height: 100%;
+  		margin-top: 2%;
+  	}
+
+	.block:hover {
+ 		opacity: 0.8;
+	}
+
+	.line {
+		border-bottom: solid 2px #1B4411;
+
+	}
+
+	.limpo {
+		text-decoration: none !important;
+		color: #848484 !important;
+	}
+
+	.active {
+	color: #1B4411 !important;		
+	}
+
+
+
+	footer{
+		background-color: #1B4411;
+		color: #ffffff;
+		text-align: center;
+		font-size: 16px;
+	}
+/*******************FONTES**************/
+	.glp {
+		color: #ffffff;
+		font-size: 20px;
+		padding: 3px;
+		letter-spacing: 3px;
+
+	}
+
+		.titulo{
+		position: fixed-top !important;
+		background-color: #1B4411 !important;
+		color: #ffffff !important;
+		text-align: center;
+	}
+
+	.title {
+		font-family: square;
+		border-top: 5px solid #C91518;
+  		padding-top: 7px;
+  		padding-bottom: 20px;
+ 		display: inline;
+  		letter-spacing: -2px;
+		font-size: 35px;
+		text-align: center;
+		color: #1B4411;
+	}
+
+	.destaques {
+		font-family: square;
+		border-top: 5px solid #C91518;
+  		padding-top: 8.5px;
+  		padding-bottom: 20px;
+ 		display: inline;
+  		letter-spacing: -2px;
+		font-size: 45px;
+		text-align: center;
+		color: #1B4411;
+	}
+</style>
+</head>
+<body>
+	<div class="container-fluid titulo">
+		<img class="logo" src="C:\xampp\htdocs\Notifique\logo certo.png">
+	</div>
+
+	<div class="spacer"	>
+	</div>
+	<div class="container">
+		<div class="row">
+			</br>
+				<div class="line">
+	</div>
+
+		<center><div class="destaques">
+		Destaques
+	</div></center>
+		<div class="spacer"	>
+	</div>
+			<div class="col-sm-6">
+		<img class="block" src="http://www.deftune.com/wp-content/uploads/2010/10/thexx-400x400.jpg">
+	</div>
+		<div class="col-sm-6">
+		<img class="block" src="http://www.deftune.com/wp-content/uploads/2010/10/thexx-400x400.jpg">
+	</div>
+	</div>
+	<div class="spacer">
+	</div>
+	<div class="line">
+	</div>
+		<center><div class="title">
+		Últimas Notícias
+	</div></center>
+	</div>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+<footer>
+</br>
+</br>
+<div class="glp"><i class="fa fa-facebook-official"></i>
+	<i class="fa fa-envelope-o" aria-hidden="true"></i>
+	<i class="fa fa-map-marker" aria-hidden="true"></i>
+	<i class="fa fa-globe" aria-hidden="true"></i>
+</div>
+
+</br>
+</br>
+<p>2017 &copy; Todos os direitos reservados</p>
+</br>
+</br>
+</br>
+</footer>
+	<div class="container-fluid navembaixo">
+			<div class="row">
+			<div class="col-sm-12">
+			<i class="fa fa-home naveg active" aria-hidden="true"></i>
+			<i class="fa fa-bars naveg" aria-hidden="true"></i>
+			<i class="fa fa-search naveg" aria-hidden="true"></i>
+			<i class="fa fa-user-o naveg" aria-hidden="true"></i>
+			</div>
+	</div>
+	</div>
+
+</body>
+</html>
